d9e6d455468a384349e0f9a9661fa211254d150d
oakhtar147 <osamaakhtar15@gmail.com>
2021-04-24 15:44:44 +0500

    react-query products

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from "react-query";
+import axios from 'axios';
+import {
+  Drawer,
+  LinearProgress,
+  Grid,
+  Badge
+} from "@material-ui/core"
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+
+import { Wrapper } from './App.styles'
+import type { CartItemType } from './types'
+import Item from './Item/Item';
 
 
+const getProducts = async (): Promise<CartItemType[]> => {
+  const response = await axios.get('https://fakestoreapi.com/products');
+  const data: CartItemType[] = response.data
+  return data;
+}
+  
 const App = () => {
+  const { isLoading, error, data } = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data);
+
+  const getTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />
+  
   return (
-    <div className="App">
-      Start
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface CartItemType {
+  id: number;
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  image: string;
+  amount: number;
+}
